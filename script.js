768a2f05a2b4c508b3cee733dff4db56f9c6ef98
MatiasC5 <cardosomatias1994@gmail.com>
2024-10-08 22:24:04 -0300

    Se termino el reloj con JS, quedan algunos ajuste en CSS

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
 const hourDisplay = document.getElementById("hour");
-const minutesDisplay = document.getElementById("minutes");
-const secondsDisplay = document.getElementById("seconds");
+const secondHand = document.querySelector(".second-hand");
 
 function setClock() {
   const currentTime = new Date();
   let hour = currentTime.getHours();
   let minutes = currentTime.getMinutes();
   let seconds = currentTime.getSeconds();
-
+  hourDisplay.textContent += hour + ":";
   setInterval(() => {
-    seconds += 1;
+    seconds++;
+
     if (seconds > 59) {
       seconds = 0;
-    } else if (seconds < 10) {
-      secondsDisplay.textContent = `0${seconds}`;
     }
-  }, 1000);
+    if (minutes > 59) {
+      minutes = 0;
+    }
+    if (hour > 23) {
+      hour = 0;
+    }
 
-  hourDisplay.textContent += hour + ":";
-  minutesDisplay.textContent += minutes + ":";
-  secondsDisplay.textContent = seconds;
+    seconds = (seconds < 10 ? "0" : "") + seconds;
+    minutes = (minutes < 10 ? "0" : "") + minutes;
+    hour = (hour < 10 ? "0" : "") + hour;
+
+    const rotationDegrees = seconds * 6;
+    secondHand.style.transform = `rotate(${rotationDegrees}deg)`;
+    const currentTime = hour + ":" + minutes + ":" + seconds;
+    hourDisplay.textContent = currentTime;
+  }, 1000);
 }
 
 setClock();
